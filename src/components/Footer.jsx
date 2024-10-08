01d9552751a442091e9d958914883c4a640a39b0
jeromie <jeromie@airmeet.com>
2024-10-08 18:38:01 +0530

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import styled from "styled-components";
 const Footer = () => {
     return(
         <Wrapper className="text-xs text-center text-slate-500">
-            Disclaimer: The calculations provided are estimates of potential rewards and may not reflect the exact points you will earn. 
-            <br />Data is as of October 1, 2024. Please note that reward rates and card features are subject to change by financial institutions, and some information may become outdated. 
-            <br />We recommend verifying details directly with your card issuer for the most accurate and up-to-date information.
+            <div className="p-3">
+                Disclaimer: The calculations provided are estimates of potential rewards and may not reflect the exact points you will earn. 
+                <br />Data is as of October 1, 2024. Please note that reward rates and card features are subject to change by financial institutions, and some information may become outdated. 
+                <br />We recommend verifying details directly with your card issuer for the most accurate and up-to-date information.
+            </div>
             <BottomFooter className="border-t border-t-slate-700 text-slate-400 p-4 mt-2">
                 All Rights Reserved 2024
             </BottomFooter>
